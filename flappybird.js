3d48652d9d3708a4d85851b1985d36db9fd26f5c
vyzu4 <99065703+vyzu4@users.noreply.github.com>
2022-10-17 22:32:01 -0700

    Update flappybird.js

--- flappybird.js
+++ flappybird.js
@@ -1,87 +1,77 @@
-document.addEventListener('DOMContentLoaded' , () => {
-    const bird = document.querySelector('.bird')
-    const gameDisplay = document.querySelector('.game-container')
-    const ground = document.querySelector('.ground-moving')
+const canvas = document.getElementById("canvas1"); //bird
+const ctx = canvas.getContext('2d');
+canvas.width = 600;
+canvas.height = 400;
 
-    let birdLeft = 220
-    let birdBottom = 100
-    let gravity = 3
-    let isGameOver = false
-    let gap = 430
+let space_pressed = false; //for space bar and clicks
+let angle = 0; //make bird move up/down
+let hue = 0; //colour
+let frame = 0; //keep track of frame count
+let score = 0; //score count (passing obstacles)
+let game_speed = 2; 
 
+const gradient = ctx.createLinearGradient(0, 0, 0, 70);
+gradient.addColorStop('0.4', '#fff');
+gradient.addColorStop('0.5', '#000');
+gradient.addColorStop('0.55', '#4040ff');
+gradient.addColorStop('0.6', '#000');
+gradient.addColorStop('0.9', '#fff');
 
-    function startGame() {
-        birdBottom -= gravity
-        bird.style.bottom = birdBottom + 'px'
-        bird.style.left = birdLeft + 'px'
+function animate(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height); //clears canvas
+    //ctx.fillRect(10, canvas.height - 90, 50, 50); //draws reactangle
+    handle_obstacles(); 
+    handle_particles();
+    bird.update();
+    bird.draw();
+
+    //score counter
+    ctx.fillStyle = 'red';
+    ctx.font = '90px Georgia';
+    ctx.strokeText(score, 450, 70);
+    ctx.fillText(score, 450, 70);
+
+    handle_collisions();
+    //if collision is detected, stop game!
+    if (handle_collisions()){
+        return;
     }
-    let gameTimerId = setInterval(startGame, 20)
 
-    function control(e) {
-        if (e.keyCode === 32) {
-            jump()
-        }
+    requestAnimationFrame(animate); //recursive animation loop
+    angle += 0.12;
+    hue++;
+    frame++;
+}
+
+animate(); //calls function
+
+window.addEventListener('keydown', function(e){ 
+    if (e.code === 'Space'){
+        space_pressed = true; //space key is pressed 
+    } 
+});
+
+window.addEventListener('keyup', function(e){ 
+    if (e.code === 'Space'){
+        space_pressed = false; //space key is released
     }
+});
 
-    function jump() {
-        if (birdBottom < 500) birdBottom += 50
-        bird.style.bottom = birdBottom + 'px'
-        console.log(birdBottom)
-    }
-    document.addEventListener('keyup', control)
+const bang = new Image();
+bang.src = 'bang.png';
 
-
-    function generateObstacle() {
-        let obstacleLeft = 500
-        let randomHeight = Math.random() * 60
-        let obstacleBottom = randomHeight
-        const obstacle = document.createElement('div')
-        const topObstacle = document.createElement('div')
-        if (!isGameOver) {
-            obstacle.classList.add('obstacle')
-            topObstacle.classList.add('topObstacle')
-        }
-        gameDisplay.appendChild(obstacle)
-        gameDisplay.appendChild(topObstacle)
-        obstacle.style.left = obstacleLeft + 'px'
-        topObstacle.style.left = obstacleLeft + 'px'
-        obstacle.style.bottom = obstacleBottom + 'px'
-        topObstacle.style.bottom = obstacleBottom + gap + 'px'
-
-        function moveObstacle() {
-            obstacleLeft -=2
-            obstacle.style.left = obstacleLeft + 'px'
-            topObstacle.style.left = obstacleLeft + 'px'
-
-            if (obstacleLeft === -60) {
-                clearInterval(timerId)
-                gameDisplay.removeChild(obstacle)
-                gameDisplay.removeChild(topObstacle)
+function handle_collisions(){
+    for(let i = 0; i < obstacles_array.length; i++){
+        //collision detected
+        if (bird.x < obstacles_array[i].x + obstacles_array[i].width &&
+            bird.x + bird.width > obstacles_array[i].x &&
+            ((bird.y < 0 + obstacles_array[i].top && bird.y + bird.height > 0) || (bird.y > canvas.height - obstacles_array[i].bottom && bird.y + bird.height < canvas.height))){
+                ctx.drawImage(bang, bird.x, bird.y, 50, 50);
+                //game over message
+                ctx.font = "25px Georgia";
+                ctx.fillStyle = 'black';
+                ctx.fillText('Game Over, Your score is ' + score, 160, canvas.height/2 - 10);
+                return true;
             }
-            if (
-                obstacleLeft > 200 && obstacleLeft < 280 && birdLeft === 220 &&
-                (birdBottom < obstacleBottom + 153 || birdBottom > obstacleBottom + gap -200)||
-                birdBottom === 0 
-                ) {
-                gameOver()
-                clearInterval(timerId)
-            }
-        }
-        let timerId = setInterval(moveObstacle, 20) 
-        if (!isGameOver) setTimeout(generateObstacle, 3000)
-
     }
-    generateObstacle()
-
-
-    function gameOver() {
-        clearInterval(gameTimerId)
-        console.log('game over')
-        isGameOver = true
-        document.removeEventListener('keyup', control)
-        ground.classList.add('ground')
-        ground.classList.remove('ground-moving')
-    }
-
-
-})
+}
